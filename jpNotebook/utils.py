4f51f7baf8c3823a442bc7efeee989824a4cdc66
VertexC <bob2420083992@gmail.com>
2018-03-31 23:38:31 -0700

    update packages

--- jpNotebook/utils.py
+++ jpNotebook/utils.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 import math
 from sklearn import linear_model
--- src/ibm/ibm.py
+++ src/ibm/ibm.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import math
 import matplotlib
-
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 
