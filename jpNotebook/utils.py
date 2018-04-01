c6ff2884c75b8a5d89522f19a1602ec0841d13a1
HaipengLi <si_shen_shang@163.com>
2018-03-31 19:45:22 -0700

    finish intersection; need to combine them into one function

--- jpNotebook/utils.py
+++ jpNotebook/utils.py
@@ -3,16 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
+ACCEPTED_MODE = ['column', 'row']
 
-def to_sti_column(cap, column_num=0):
+
+def to_sti(cap, mode='column', column_or_row_num=0):
     """
-    Extract STI of middle column from a video
+    Extract STI of given column or row from a video
     :param cap: cv2.VideoCapture object
+    :param mode: anyone in ACCEPTED_MODE
+    :param column_or_row_num: specify which column or row should be extracted
     :return: STI image
     """
     if not cap.isOpened():
         print("The video is not opened!")
         return None
+
+    if mode.lower() not in ACCEPTED_MODE:
+        raise ValueError('Unknown mode: {}. Accepted List: {}'.format(mode, ACCEPTED_MODE))
 
     # get necessary information
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -20,17 +27,29 @@ def to_sti_column(cap, column_num=0):
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    sti_column = np.ndarray((height, frame_count, 3), dtype=np.uint8)
+    height_or_width = 0
+    if mode == 'column':
+        height_or_width = height
+    elif mode == 'row':
+        height_or_width = width
+
+    sti = np.ndarray((height_or_width, frame_count, 3), dtype=np.uint8)
     i = 0
     while True:
         ret, frame = cap.read()
         if not ret: break
         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        selected_column = frame[:, column_num, :]
-        sti_column[:, i, :] = selected_column
+        if mode == 'column':
+            selected_column_or_row = frame[:, column_or_row_num, :]
+        elif mode == 'row':
+            selected_column_or_row = frame[column_or_row_num, :, :]
+        else:
+            # should not happen
+            selected_column_or_row = 0
+        sti[:, i, :] = selected_column_or_row
         i += 1
 
-    return sti_column
+    return sti
 
 def bgr_to_rg(image_bgr):
     # convert type from uint8 to float
@@ -40,9 +59,9 @@ def bgr_to_rg(image_bgr):
     # + 0.00000001 to avoid divided by zero
     bgr_sum = (image_bgr_copy[:, :, 0] + image_bgr_copy[:, :, 1] + image_bgr_copy[:, :, 2] + 0.0000001)
     # red
-    image_rg[:, :, 0] = image_bgr_copy[:, :, 2] / bgr_sum
+    image_rg[:, :, 0] = image_bgr_copy[:, :, 2]
     # green
-    image_rg[:, :, 1] = image_bgr_copy[:, :, 1] / bgr_sum
+    image_rg[:, :, 1] = image_bgr_copy[:, :, 1]
     return image_rg
 
 def cal_hist_rg(image_rg, bin_size):
@@ -63,8 +82,15 @@ def normalize(hist):
     total = np.sum(hist)
     return np.zeros(hist.shape) if total == 0 else hist / total
 
-def intersection(sti_columns_rg, threshold):
-    width, height, frame_count = sti_columns_rg.shape[:3]
+def intersection(stis_rg, threshold):
+    """
+    Use STI made of columns to generate all the transitions (position, time).
+    Though is function is written for STI of columns, but it also works for rows
+    :param stis_rg: sti with shape (width, height, frame_count, 2)
+    :param threshold: 0 ~ 1, the larger, the more strict
+    :return: wipe_cols, wipe_frames, can be drawn using scatter()
+    """
+    width, height, frame_count = stis_rg.shape[:3]
     bin_size = 1 + math.floor(math.log(height, 2))
 
     H = np.ndarray((width, frame_count, bin_size, bin_size))
@@ -72,13 +98,13 @@ def intersection(sti_columns_rg, threshold):
     # compute hist for one column from STI
     for col in range(width):
         for f in range(frame_count):
-            H[col, f] = normalize(cal_hist_rg(sti_columns_rg[col, :, f:f + 1, :], bin_size))
+            H[col, f] = normalize(cal_hist_rg(stis_rg[col, :, f:f + 1, :], bin_size))
 
     # create np.1darray for histogram intersection
     I = np.ndarray((width, frame_count - 1))
 
     # contruct the column * frame graph
-    wipe_cols = []
+    wipe_positions = []
     wipe_frames = []
 
     # collect all col, frame of wipe transitions
@@ -87,31 +113,54 @@ def intersection(sti_columns_rg, threshold):
         for f in range(I.shape[1]):
             # I[i] intersects histogram of frames at time i+1 and i
             I[col, f] = np.sum(np.minimum(H[col, f + 1], H[col, f]))
-            # TODO: add cooldown time to avoid several continuous frames are all regraded as wipe
             if I[col, f] < threshold:
-                wipe_cols.append(col)
+                wipe_positions.append(col)
                 wipe_frames.append(f)
     #             print("Wipe at: {}".format(f + 1))
 
-    wipe_cols = np.array(wipe_cols).reshape(-1, 1)
+    number = len(wipe_positions)
+    print("The number of transition detected: {}".format(number))
+    if number < width // 2:
+        print("Warning: too few valid positions are detected. The result may be inaccurate.")
+        print("Try to increase the threshold")
+    if number > width * 2:
+        print("Warning: too many valid positions are detected. The result may be inaccurate.")
+        print("Try to decrease the threshold")
+
+    wipe_positions = np.array(wipe_positions).reshape(-1, 1)
     wipe_frames = np.array(wipe_frames).reshape(-1, 1)
 
-    return wipe_cols, wipe_frames
+    return wipe_positions, wipe_frames
 
-def linear_regression_column(wipe_cols, wipe_frames, width):
+def linear_regression_column(wipe_positions, wipe_frames, width_or_height):
+    """
+    Use linear regression to predict the start / end of the transition
+    :param wipe_positions: the col or row that a wipe happens on
+    :param wipe_frames: the frame that a wipe happens at
+    :param width_or_height: width when sti is made of columns
+    :return:
+    """
+
+    # check the validity of data
+    if not wipe_positions.shape == wipe_frames.shape:
+        raise ValueError("The shape of first two inputs must match")
+    if wipe_positions.shape[0] == 0:
+        raise ValueError("The number of data should be greater than zero")
+
     from sklearn import linear_model
-
+    from sklearn.metrics import mean_squared_error
     regr = linear_model.LinearRegression()
-    regr.fit(wipe_cols, wipe_frames)
+    regr.fit(wipe_positions, wipe_frames)
+    positions_test = np.array([0, width_or_height - 1]).reshape(-1, 1)
 
-    cols_test = np.array([0, width - 1]).reshape(-1, 1)
-    frames_pred = regr.predict(cols_test)
+    frames_pred = regr.predict(positions_test)
 
-    plt.scatter(wipe_cols, wipe_frames, color='black', linewidths=1)
-    plt.plot(cols_test, frames_pred, color='blue', linewidth=3)
+    plt.scatter(wipe_positions, wipe_frames, color='black', linewidths=1)
+    plt.plot(positions_test, frames_pred, color='blue', linewidth=3)
 
     # plt.xticks(())
     # plt.yticks(())
 
     plt.show()
     print("start/end frame: {}".format(np.round(sorted(frames_pred.flatten()))))
+    print("Mean Square Error: {}".format(mean_squared_error(wipe_frames, regr.predict(wipe_positions))))
