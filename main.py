30e427965831233e6de5b77405cc79d662b9b4a4
VertexC <bob2420083992@gmail.com>
2018-04-01 23:41:42 -0700

    add pixel copy into main

--- main.py
+++ main.py
@@ -1,11 +1,127 @@
+from src.model import *
+from src.util import utils
 import tkinter as tk
 from tkinter import ttk
-from PIL import ImageTk, Image
+from tkinter import filedialog
+from tkinter import messagebox
+import cv2
+import numpy as np
+import math
+import matplotlib
+from PIL import Image, ImageTk
 
+
+
+# global variable
+video_file = None
+sample_mode = None
 
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
+
+
+class CopyPixelPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+
+        rows = 0
+        while rows < 60:
+            self.rowconfigure(rows, weight=1)
+            self.columnconfigure(rows, weight=1)
+            rows += 1
+
+        # File Selector
+        path_var = tk.StringVar()
+
+        def select_file():
+            global video_file
+            video_file = filedialog.askopenfilename()
+
+            def path_shorten(path):
+                while len(path) > 40:
+                    i = 0
+                    for i in range(len(path)):
+                        if path[i] == '/':
+                            path = path[i + 1:]
+                            break
+                    if i == len(path) - 1:
+                        return path
+                return path
+
+            path_var.set("SOMEPATH/" + path_shorten(video_file))
+            # print(video_file)
+
+        tk.Button(self, text='Slect Video File',
+                  width=4, height=1,
+                  command=select_file).grid(row=5, column=0, rowspan=1, columnspan=4, sticky='NESW')
+
+        # File Selected Text
+        tk.Label(self, textvariable=path_var,
+                 width=40, height=2).grid(row=5, column=4, rowspan=1, columnspan=40, sticky='NESW')
+
+        # Sampling Choice
+        var = tk.StringVar()
+
+        def samling_mode_set():
+            global sample_mode
+            sample_mode = var.get()
+
+        tk.Label(self,
+                 text="Sampling Choice",
+                 # bg='green',
+                 font=('Arial', 18),
+                 width=3, height=1
+                 ).grid(row=19, column=0, rowspan=1, columnspan=4, sticky='NESW')
+
+        row_choice = tk.Radiobutton(self, text='MidRow Sampling',
+                                    variable=var, value='row',
+                                    command=samling_mode_set)
+        row_choice.grid(row=20, column=0, rowspan=3, columnspan=4, sticky="NESW", padx=0)
+        col_choice = tk.Radiobutton(self, text='MidCol Sampling',
+                                    variable=var, value='col',
+                                    command=samling_mode_set)
+        col_choice.grid(row=23, column=0, rowspan=3, columnspan=4, sticky="NESW", padx=0)
+
+        # label to show sti image
+        IMAGE_WIDTH = 200
+        IMAGE_HEIGHT = 180
+        loading_img = cv2.imread(filename='./image/loading.jpg', flags=cv2.IMREAD_COLOR)
+        loading_img = cv2.resize(loading_img, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_CUBIC)
+        loading_img = Image.fromarray(loading_img, "RGB")
+        loading_img = ImageTk.PhotoImage(loading_img)
+
+        copypixel_sti_label = tk.Label(self)
+        copypixel_sti_label.grid(row=19, column=10, rowspan=20, columnspan=30, sticky="NESW", padx=0)
+
+        # Start detection
+        def detection():
+            if video_file == None:
+                # warning dialog
+                messagebox.showinfo("Warning", "Please select a video file to detect.")
+                return
+            if sample_mode == None:
+                # warning dialog
+                messagebox.showinfo("Warning", "Please select a sampling mode.")
+                return
+            # set loading image
+            copypixel_sti_label.config(image=loading_img)
+            copypixel_sti_label.image = loading_img
+            # pixel sampling
+            if sample_mode == "row":
+                sti = utils.pixel_copy_sti(video_file, use_row=True)
+            else:
+                sti = utils.pixel_copy_sti(video_file, use_row=False)
+            # set sti image
+            sti_img = cv2.resize(sti, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_CUBIC)
+            sti_img = Image.fromarray(sti_img, "RGB")
+            sti_img = ImageTk.PhotoImage(sti_img)
+            copypixel_sti_label.config(image=sti_img)
+            copypixel_sti_label.image = sti_img
+
+        tk.Button(self,
+                  text="Start Detection",
+                  width=10, height=2, command=detection).grid(row=35, column=0, rowspan=1, columnspan=4, sticky="NESW")
 
 
 class NoteBook(tk.Frame):
@@ -16,7 +132,8 @@ class NoteBook(tk.Frame):
         nb.pack(side="top", fill="both", expand=True)
 
         # add Page to Notebook
-
+        copypixel_page = CopyPixelPage(nb)
+        nb.add(copypixel_page, text='Copy Pixel')
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -38,6 +155,7 @@ class MainApplication(tk.Frame):
 
         body = NoteBook(self)
         body.grid(row=20, column=0, rowspan=60, columnspan=80, sticky='NESW')
+
 
 
 if __name__ == "__main__":
